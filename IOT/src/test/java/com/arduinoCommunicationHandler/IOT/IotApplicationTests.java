7453091d89eeeccbbfabaf966be7d47a77c88a58
John <1273641590@qq.com>
2021-07-02 19:07:15 +0800

    dev: 创建了springboot项目

---
+++ IOT/src/test/java/com/arduinoCommunicationHandler/IOT/IotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arduinoCommunicationHandler.IOT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
